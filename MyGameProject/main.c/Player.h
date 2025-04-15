633d31dd06b92a555aa261f6a4a4d2840a88d768
gydud0819 <pstar0819@naver.com>
2025-04-15 21:38:39 +0900

    (2025-04-15) cpp변경 및 구조체 변경

--- MyGameProject/main.c/Player.h
+++ MyGameProject/main.c/Player.h
@@ -5,32 +5,32 @@
 * 구조체를 정의할 때 어떤 것을 먼저 선언해야하는지 잘 생각해야한다. 그렇지 않으면 먼저 선언한 구조체가 인식을 하지 못하는 경우가 발생하여 오류가 날 수 있다. 
 */
 
-typedef struct _POS		// 위치 구조체
+struct Pos		// 위치 구조체
 {
 	int posX;
 	int posY;
-}Pos;
+};
 
-typedef struct _EXIT	// 출구 구조체
+struct Exit	// 출구 구조체
 {
 	Pos exitPos;
 	bool exit;
-}Exit;
+};
 
-typedef struct _STAGE
+struct Stage
 {
 	Pos stagePos;
 	bool nextStage;
-}Stage;
+};
 
-typedef struct _ITEM	// 아이템 구조체
+struct Item	// 아이템 구조체
 {
 	char* itemShape;
 	Exit itemExit;
 
-}Item;
+};
 
-typedef struct _PLAYER	// 플레이어 구조체
+struct Player	// 플레이어 구조체
 {
 	char* playerShape;
 	Pos playerPos;
@@ -41,7 +41,9 @@ typedef struct _PLAYER	// 플레이어 구조체
 	int CurrentStage;
 	int prevPosX;      
 	int prevPosY;      
-}Player;
+
+	void MovePlayer();
+};
 
 void MovePlayer(Player* playerptr, const Exit* exitptr);		// 플레이어가 콘솔창 내에서 움직이는 함수
 
---
+++ MyGameProject/main.c/main.cpp
@@ -0,0 +1,64 @@
+#include "MapBoder.h"
+#include "Console.h"
+#include "Player.h"
+
+
+extern int Map1[가로][세로];	// 미로의 크기
+extern int Map2[가로2][세로2];	// 미로의 크기
+
+int main()
+{
+	COORD stagePos = { 50,0 };
+	//ShowStage1(Stage1, stagePos);			// 미로 내부가 보이는 함수
+	ShowClearStage1(Stage1, stagePos);			// 미로 내부가 보이지 않는 함수
+	//ShowStage2();			// 두번째 스테이지 맵
+	HideCursor();
+
+	int playerX = 11;	// 플레이어의 시작 위치
+	int playerY = 2;	// 플레이어의 시작 위치
+
+	SetCurPosition(playerX, playerY);
+	printf("옷");
+
+	Player player;
+	player.playerShape = "옷";
+	player.playerPos.posX = 11;
+	player.playerPos.posY = 2;
+	player.mazeExit = false;
+	player.heartCount = 0;
+	player.starCount = 0;	// 2번째 스테이지 아이템 초기화
+	player.CurrentStage = 1;
+
+	Exit exit;
+	exit.exitPos.posX = 29;
+	exit.exitPos.posY = 2;
+	exit.exit = false;
+
+	SetCurPosition(player.playerPos.posX, player.playerPos.posY);
+	SetColor(0, 2);
+	printf("옷");
+	SetColor(0, 15);
+		
+	while (true)
+	{
+		int prevX = player.playerPos.posX;
+		int prevY = player.playerPos.posY;
+
+		// ▼▼▼ 부분 화면 갱신 ▼▼▼
+		if (prevX != player.playerPos.posX || prevY != player.playerPos.posY) 
+		{
+			SetCurPosition(prevX, prevY);
+			printf(" "); // 이전 위치 지우기
+			SetCurPosition(player.playerPos.posX, player.playerPos.posY);
+			SetColor(0, 2);
+			printf("옷");
+			SetColor(0, 15);
+		}
+				
+		MovePlayer(&player, &exit);
+
+		Sleep(50);
+		
+
+	}
+}
